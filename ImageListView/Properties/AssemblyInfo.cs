7546a883a0bd1595ee5a43eff8fcfe5a649fb4ed
Steve Hall <stigzler@msn.com>
2023-08-05 14:16:50 +0100

    Before changes to         public ImageListViewItemCollection Items

--- ImageListView/Properties/AssemblyInfo.cs
+++ ImageListView/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-// ImageListView - A listview control for image files
+﻿// ImageListView - A listview control for image files
 // Copyright (C) 2009 Ozgur Ozcitak
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,9 +24,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ImageListView")]
 [assembly: AssemblyDescription("A ListView like control for displaying image files with asynchronously loaded thumbnails.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("?zg?r ?z??tak")]
+[assembly: AssemblyCompany("Özgür Özçıtak")]
 [assembly: AssemblyProduct("ImageListView")]
-[assembly: AssemblyCopyright("Copyright ?  2013 ?zg?r ?z??tak")]
+[assembly: AssemblyCopyright("Copyright ©  2013 Özgür Özçıtak")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("13.8.2.15")]
-[assembly: AssemblyFileVersion("13.8.2.15")]
+[assembly: AssemblyVersion("13.8.2.4")]
+[assembly: AssemblyFileVersion("13.8.2.4")]
